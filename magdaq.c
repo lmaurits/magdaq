28eb4ee869e227f199f4782ab1e65da5f2e8b79d
Luke Maurits <luke@maurits.id.au>
2021-04-25 21:00:57 +0200

    Read temperature from DS18B20 and send this along with frequency count.

--- magdaq.c
+++ magdaq.c
@@ -1,8 +1,22 @@
-#define F_CPU	14745600UL
-#define BAUD	9600
+#define F_CPU  14745600UL
+#define BAUD   9600
+
+#define DELAY_A 6
+#define DELAY_B 64
+#define DELAY_C 60
+#define DELAY_D 10
+#define DELAY_E 9
+#define DELAY_F 55
+#define DELAY_G 0
+#define DELAY_H 480
+#define DELAY_I 70
+#define DELAY_J 410
+#define RELEASE_BUS DDRD = 0
+#define DRIVE_BUS_LOW DDRD |= 0x40; PORTD &= 0x3F;
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include <util/setbaud.h>
 
 // Global vars
@@ -41,6 +55,58 @@ void init_multiplexer() {
     PORTB = 0;
 }
 
+// Temperature sensor functions
+uint8_t onewire_reset() {
+    uint8_t presence;
+    DRIVE_BUS_LOW;
+    _delay_us(DELAY_H);
+    RELEASE_BUS;
+    _delay_us(DELAY_I);
+    presence = PIND &= 0x40;
+    _delay_us(DELAY_J);
+    return presence;
+}
+
+void onewire_write_byte(uint8_t x) {
+    uint8_t i = 8;
+    while(i) {
+  if(x & 0x01) {
+            DRIVE_BUS_LOW;
+            _delay_us(DELAY_A);
+            RELEASE_BUS;
+            _delay_us(DELAY_B);
+  } else {
+            DRIVE_BUS_LOW;
+            _delay_us(DELAY_C);
+            RELEASE_BUS;
+            _delay_us(DELAY_D);
+  }
+        x >>= 1;
+        i--;
+    }
+}
+
+uint8_t onewire_read_byte() {
+    uint8_t x, tmp;
+    x = 0;
+    uint8_t i = 8;
+    while(i) {
+        x >>= 1;
+        DRIVE_BUS_LOW;
+        _delay_us(DELAY_A);
+  RELEASE_BUS;
+        _delay_us(DELAY_E);
+  tmp = PIND &= 0x40;
+        _delay_us(DELAY_F);
+  if(tmp) {
+      x |= 0x80;
+  }
+        i--;
+    }
+    return x;
+}
+
+
 // Timer functions
 // Timer 0 is the 8 bit timer connected to the 10Hz output of the TCXO
 // Timer 1 is the 16 bit timer connected to the magnetometer(s)
@@ -63,9 +129,6 @@ void init_timers() {
 
 void send_datapoint() {
     
-    uart_send_byte(capture_tcnt1 & 0xFF);
-    uart_send_byte((capture_tcnt1 >> 8) & 0xFF);
-    uart_send_byte(capture_overflows);
     
     uart_send_byte(0xAA);
     uart_send_byte(0xBB);
@@ -91,18 +154,32 @@ ISR(TIMER1_OVF_vect) {
 // Main
 
 int main() {
+    uint8_t high, low;
     cli();
     init_uart();
     init_multiplexer();
     init_timers();
     sei();
     while(1) {
-       if(int_flag != 0) {
-           send_datapoint();
-           int_flag = 0;
-       }
+        if(int_flag != 0) {
+            int_flag = 0;
+            // Read results of previous temperature conversion
+            onewire_reset();
+            onewire_write_byte(0xCC); // SKIP ROM
+            onewire_write_byte(0xBE); // READ SCRATCHPAD
+            low = onewire_read_byte();
+            high = onewire_read_byte();
+            // Send measurements
+            uart_send_byte(capture_tcnt1 & 0xFF);
+            uart_send_byte((capture_tcnt1 >> 8) & 0xFF);
+            uart_send_byte(capture_overflows);
+            uart_send_byte(low);
+            uart_send_byte(high);
+            // Initiate next temperature conversion
+            onewire_reset();
+            onewire_write_byte(0xCC); // SKIP ROM
+            onewire_write_byte(0x44); // CONVERT T
+        }
     }
-
     return 0; 
 }
-
